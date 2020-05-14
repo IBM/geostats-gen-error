d640f46629ce6443aa99d09df307020ef1de4382
Breno W. S. R. de Carvalho <breno.william.santos@ibm.com>
2020-05-14 11:16:13 -0300

    Fix typos

--- newzealand.jl
+++ newzealand.jl
@@ -14,7 +14,7 @@ Random.seed!(123)
 # estimators of generalization error
 error_cv( m, p, k, ℒ) = error(PointwiseLearn(m), p, CrossValidation(k, loss=ℒ))
 error_bcv(m, p, r, ℒ) = error(PointwiseLearn(m), p, BlockCrossValidation(r, loss=ℒ))
-error_drv(m, p, k, σ=2.0, ℒ) = error(PointwiseLearn(m), p, DensityRatioValidation(k, loss=ℒ, estimator=LSIF(σ=σ,b=10)))
+error_drv(m, p, k, ℒ, σ=2.0) = error(PointwiseLearn(m), p, DensityRatioValidation(k, loss=ℒ, estimator=LSIF(σ=σ,b=10)))
 
 # true error (known labels)
 function error_empirical(m, p, ℒ)
@@ -30,12 +30,12 @@ function experiment(m, p, σ, rᵦ, k, ℒ)
     # try different error estimates
     cv  = error_cv(m, p, k, ℒ)
     bcv = error_bcv(m, p, rᵦ, ℒ)
-    drv = error_drv(m, p, k, ℒ)
+    drv = error_drv(m, p, k, ℒ, σ)
 
     # true error
     actual = error_empirical(m, p, ℒ)
     map(outputvars(task(p))) do var
-      (rᵦ=rᵦ, k=k, CV=cv[var], BCV=bcv[var], DRV=drv[var],
+      (rᵦ=rᵦ, k=k, CV=cv[var], BCV=bcv[var], DRV=drv[var], DRV_SIGMA=σ,
        ACTUAL=actual[var], MODEL=info(m).name, TARGET=var)
     end
 end
@@ -45,8 +45,10 @@ end
 # -------------
 
 # read/clean raw data
+println("Read/clean raw data")
 df = CSV.read("data/new_zealand/logs_no_duplicates.csv")
 df = df[:,[:X,:Y,:Z,:GR,:SP,:DENS,:NEUT,:DTC,:FORMATION,:ONSHORE]]
+numeric=[:GR,:SP,:DENS,:NEUT,:DTC]
 dropmissing!(df)
 categorical!(df, :FORMATION)
 categorical!(df, :ONSHORE)
@@ -69,8 +71,8 @@ onoff = groupby(Ω, :ONSHORE)
 ordered = sortperm(onoff[:values], rev=true)
 Ωs, Ωt = onoff[ordered]
 
-# Ωs = sample(Ωs, 100)
-# Ωt = sample(Ωt, 100)
+Ωs = sample(Ωs, 10000)
+Ωt = sample(Ωt,  2000)
 data_Ωs = OrderedDict{Symbol,AbstractArray}(v => Ωs[v] for (v,V) in variables(Ωs))
 data_Ωt = OrderedDict{Symbol,AbstractArray}(v => Ωt[v] for (v,V) in variables(Ωt))
 
@@ -86,6 +88,7 @@ k  = length(GeoStats.partition(Ωs, BlockPartitioner(rᵦ)))
 # ---------------
 # CLASSIFICATION
 # ---------------
+println("Classification")
 t = ClassificationTask((:GR,:SP,:DENS,:DTC,:NEUT), :FORMATION)
 p = LearningProblem(new_Ωs, new_Ωt, t)
 
@@ -106,7 +109,7 @@ progress = Progress(length(iterator), "New Zealand classification:")
 skip = e -> (println("Skipped: $e"); missing)
 
 # perform experiments
-cresults = progress_pmap(iterator, progress=progress,
+@time cresults = progress_pmap(iterator, progress=progress,
                         on_error=skip) do (m, σ)
   experiment(m, p, σ, rᵦ, k, ℒ)
 end
@@ -114,6 +117,7 @@ end
 # -----------
 # REGRESSION
 # -----------
+println("Regression")
 @load LinearRegressor pkg="MLJLinearModels"
 @load DecisionTreeRegressor pkg="DecisionTree"
 @load RandomForestRegressor pkg="DecisionTree"
@@ -126,10 +130,10 @@ vrange = [:GR]
 
 # experiment iterator and progress
 iterator = Iterators.product(mrange, σrange, vrange)
-progress = Progress(length(iterator), "New Zealand classification:")
+progress = Progress(length(iterator), "New Zealand regression:")
 
 # perform experiments
-rresults = progress_pmap(iterator, progress=progress,
+@time rresults = progress_pmap(iterator, progress=progress,
                         on_error=skip) do (m, σ, v)
   t = RegressionTask(numeric[numeric .!= v], v)
   p = LearningProblem(new_Ωs, new_Ωt, t)
@@ -141,6 +145,7 @@ end
 all = vcat(cresults, rresults)
 res = DataFrame(skipmissing(Iterators.flatten(all)))
 
+println("Saving results")
 # save all results to disk
 fname = joinpath(@__DIR__,"results","newzealand.csv")
 CSV.write(fname, res)
