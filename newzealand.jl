ffdffb61992b3c453e6f0e120d42a6604f73f3b1
Júlio Hoffimann <julio.hoffimann@gmail.com>
2020-05-18 21:51:19 -0300

    Update newzealand.jl

--- newzealand.jl
+++ newzealand.jl
@@ -15,7 +15,7 @@ Random.seed!(123)
 # estimators of generalization error
 error_cv( m, p, k, ℒ) = error(PointwiseLearn(m), p, CrossValidation(k, loss=ℒ))
 error_bcv(m, p, r, ℒ) = error(PointwiseLearn(m), p, BlockCrossValidation(r, loss=ℒ))
-error_drv(m, p, k, σ, ℒ) = error(PointwiseLearn(m), p, DensityRatioValidation(k, loss=ℒ, estimator=LSIF(σ=σ,b=10)))
+error_drv(m, p, k, ℒ) = error(PointwiseLearn(m), p, DensityRatioValidation(k, loss=ℒ, estimator=LSIF(σ=2.0,b=10)))
 
 # true error (known labels)
 function error_empirical(m, p, ℒ)
@@ -27,17 +27,17 @@ function error_empirical(m, p, ℒ)
   Dict(results)
 end
 
-function experiment(m, p, σ, r, k, ℒ)
+function experiment(m, p, r, k, ℒ)
     # try different error estimates
-    cv  = error_cv(m, p, k, ℒ)
+    cv  = error_cv( m, p, k, ℒ)
     bcv = error_bcv(m, p, r, ℒ)
-    drv = error_drv(m, p, k, σ, ℒ)
+    drv = error_drv(m, p, k, ℒ)
 
     # actual error (unhide labels)
     actual = error_empirical(m, p, ℒ)
 
     map(outputvars(task(p))) do var
-      (σ=σ, CV=cv[var], BCV=bcv[var], DRV=drv[var],
+      (CV=cv[var], BCV=bcv[var], DRV=drv[var],
        ACTUAL=actual[var], MODEL=info(m).name, TARGET=var)
     end
 end
@@ -78,19 +78,9 @@ onoff = groupby(Ω, :ONSHORE)
 order = sortperm(onoff[:values], rev=true)
 Ωs, Ωt = onoff[order]
 
-# sample the data
-# Ωs = sample(Ωs, 10000)
-# Ωt = sample(Ωt,  2000)
-
-# materialize the views (to avoid indirections to memory)
-Ds = OrderedDict{Symbol,AbstractArray}(v => Ωs[v] for (v,V) in variables(Ωs))
-Dt = OrderedDict{Symbol,AbstractArray}(v => Ωt[v] for (v,V) in variables(Ωt))
-Ωs = PointSetData(Ds, coordinates(Ωs))
-Ωt = PointSetData(Dt, coordinates(Ωt))
-
-# set block side and equivalent number of folds
-r = 500.
-k = length(GeoStats.partition(Ωs, BlockPartitioner(rᵦ)))
+# set block sides and equivalent number of folds
+r = (10000.,10000.,500.)
+k = length(GeoStats.partition(Ωs, BlockPartitioner(r)))
 
 # ---------------
 # CLASSIFICATION
@@ -98,17 +88,20 @@ k = length(GeoStats.partition(Ωs, BlockPartitioner(rᵦ)))
 t = ClassificationTask(logs, :FORMATION)
 p = LearningProblem(Ωs, Ωt, t)
 
-@load DecisionTreeClassifier
-@load KNNClassifier
+@load LDA pkg="MultivariateStats"
+@load ConstantClassifier pkg="MLJModels"
+@load KNNClassifier pkg="NearestNeighbors"
+@load EvoTreeClassifier pkg="EvoTrees"
+@load GaussianNBClassifier pkg="NaiveBayes"
 
 ℒ = Dict(:FORMATION => MisclassLoss())
 
 # parameter ranges
-mrange = [DecisionTreeClassifier(),KNNClassifier()]
-σrange = [1.,5.,10.,15.,20.,25.]
+mrange = [LDA(), ConstantClassifier(), KNNClassifier(),
+          EvoTreeClassifier(), GaussianNBClassifier()]
 
 # experiment iterator and progress
-iterator = Iterators.product(mrange, σrange)
+iterator = Iterators.product(mrange)
 progress = Progress(length(iterator), "New Zealand classification:")
 
 # return missing in case of failure
@@ -116,8 +109,8 @@ skip = e -> (println("Skipped: $e"); missing)
 
 # perform experiments
 cresults = progress_pmap(iterator, progress=progress,
-                        on_error=skip) do (m, σ)
-  experiment(m, p, σ, r, k, ℒ)
+                         on_error=skip) do (m,)
+  experiment(m, p, r, k, ℒ)
 end
 
 # -----------
@@ -126,7 +119,7 @@ end
 @load LinearRegressor pkg="MLJLinearModels"
 @load DecisionTreeRegressor pkg="DecisionTree"
 @load RandomForestRegressor pkg="DecisionTree"
-@load KNNRegressor
+@load KNNRegressor pkg="NearestNeighbors"
 
 # parameter ranges
 mrange = [LinearRegressor(),DecisionTreeRegressor(),
@@ -139,11 +132,11 @@ progress = Progress(length(iterator), "New Zealand regression:")
 
 # perform experiments
 rresults = progress_pmap(iterator, progress=progress,
-                        on_error=skip) do (m, σ, v)
+                         on_error=skip) do (m, v)
   t = RegressionTask(logs[logs .!= v], v)
   p = LearningProblem(Ωs, Ωt, t)
   ℒ = Dict(v => L2DistLoss())
-  experiment(m, p, σ, r, k, ℒ)
+  experiment(m, p, r, k, ℒ)
 end
 
 # merge all results into dataframe
