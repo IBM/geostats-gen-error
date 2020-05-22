d09c728e918ce32d930070cf51aa05677ec9d379
Júlio Hoffimann <julio.hoffimann@gmail.com>
2020-05-22 15:34:43 -0300

    Update results.newzealand.jl

--- results/newzealand.jl
+++ results/newzealand.jl
@@ -24,31 +24,49 @@ end
 # load results table
 df = CSV.read(joinpath(@__DIR__,"newzealand.csv"))
 
-# name columns
-serror = 3
-terror = 4
-errors = 5:7
+# index of important columns
+serror, terror, errors = 3, 4, 5:7
+
+# enable/disable LaTeX output
+print_latex = true
 
 # highlight table entries
-best = Highlighter((d, i, j) -> begin
+function best(d, i, j)
   k = argmin([abs(d[i,k] - d[i,terror]) for k in errors])
   j ∈ errors && errors[k] == j
-  end, bold=true, foreground=:blue
-)
-worst = Highlighter((d, i, j) -> begin
+end
+function worst(d, i, j)
   k = argmax([abs(d[i,k] - d[i,terror]) for k in errors])
   j ∈ errors && errors[k] == j
-  end, bold=true, foreground=:red
-)
-actual = Highlighter((d, i, j) ->
-  j ∈ serror ∪ terror, foreground=:dark_gray
-)
+end
+function target(d, i, j)
+  j == terror
+end
+hb  = Highlighter(best, bold=true, foreground=:blue)
+hw  = Highlighter(worst, bold=true, foreground=:red)
+ht  = Highlighter(target, bold=true)
+hbl = LatexHighlighter(best, ["color{NavyBlue}","textbf"])
+hwl = LatexHighlighter(worst, ["color{Maroon}","textbf"])
+htl = LatexHighlighter(target, ["textbf"])
 
 for g in groupby(df, :VARIABLE)
-  pretty_table(g, nosubheader=true, crop=:none,
+  # variable name and number of models
+  v = g[1,:VARIABLE]
+  n = size(g, 1)
+
+  pretty_table(g, nosubheader=true, crop=:none, vlines=[0,2,4,7],
                alignment=[:c,:c,:r,:r,:r,:r,:r],
                formatters=ft_round(3, serror ∪ terror ∪ errors),
-               highlighters=(best, worst, actual))
+               highlighters=(hb, hw, ht))
+
+  if print_latex
+    open("newzealand-$v.tex", "w") do io
+      pretty_table(io, g, backend=:latex, tf=latex_simple,
+                   nosubheader=true, alignment=:c, vlines=[2,4],
+                   formatters=ft_round(3, serror ∪ terror ∪ errors),
+                   highlighters=(hbl, hwl, htl))
+    end
+  end
 
   # model ranking based on each method
   ranks = map([:TARGET,:CV,:BCV,:DRV]) do err
@@ -57,11 +75,18 @@ for g in groupby(df, :VARIABLE)
   end
   r = DataFrame(ranks)
 
+  colorval(k) = color(k, n)
   hs = Tuple([Highlighter((d, i, j) -> d[i,j] == r[k,Symbol("TARGET RANK")],
-                          background=color(k,size(r,1)), foreground=:black) for k in 1:size(r,1)])
-  pretty_table(r, nosubheader=true, crop=:none,
-               highlighters=hs, alignment=:c)
-end
+             background=colorval(k), foreground=:black) for k in 1:n])
+  pretty_table(r, nosubheader=true, crop=:none, alignment=:c, highlighters=hs)
 
-# pretty_table(df, backend=:latex, tf=latex_simple,
-#              nosubheader=true, formatters=ft_round(2,3:6))
+  if print_latex
+    colorstr(k) = "cellcolor[RGB]{" * join(color(k, n), ",") * "}"
+    hs = Tuple([LatexHighlighter((d, i, j) -> d[i,j] == r[k,Symbol("TARGET RANK")],
+               [colorstr(k), "color{white}", "textbf"]) for k in 1:n])
+    open("newzealand-rank-$v.tex", "w") do io
+      pretty_table(io, r, backend=:latex, tf=latex_simple,
+                   nosubheader=true, alignment=:c, highlighters=hs)
+    end
+  end
+end
